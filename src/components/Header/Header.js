fc884e6a2cc7a7a606b99b6622d0d04e82b3aea8
NMRayhan <nurmdrayhan97@gmail.com>
2021-02-28 04:02:40 +0600

    Card Fixed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,12 +5,14 @@ import './Header.css'
 const Header = () => {
     return (
         <header className="header">
-            <a href="#"><img src={logo} alt="emma-john"/></a>
-            <nav className="d-flex flex-row">
-                <a href="/shop">Shop</a>
-                <a href="/review">Order Review</a>
-                <a href="/manage">Inventory Management</a>
-            </nav>
+            <a href="#"><img src={logo} alt="emma-john" /></a>
+            <div className="bg-dark">
+                <nav className="d-flex flex-row container bg-dark">
+                    <a href="/shop">Shop</a>
+                    <a href="/review">Order Review</a>
+                    <a href="/manage">Inventory Management</a>
+                </nav>
+            </div>
         </header>
     );
 };
